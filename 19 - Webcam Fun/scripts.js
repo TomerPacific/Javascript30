9cf81c3bc7002bb080ea754eb273773f3960047f
Tomer Ben Rachel <tomerpacific@gmail.com>
2017-11-08 13:51:48 +0200

    Finished 19th Project

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,92 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+	navigator.mediaDevices.getUserMedia({video:true, audio:false})
+		.then(localMediaStream => {
+			video.src = window.URL.createObjectURL(localMediaStream);
+			video.play();
+		}).catch(err=> {
+			console.error(err)
+		});
+}
+
+function paintToCanvas() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+		let pixels = ctx.getImageData(0,0, width, height);
+		//call either redEffect, rgbSplit or greenScreen
+		//pixels = redEffect(pixels);
+		// pixels = rgbSplit(pixels);
+		// pixels = greenScreen(pixels);
+		ctx.globalAlpha = 0.1;
+		ctx.putImageData(pixels, 0, 0);
+	}, 16);
+}
+
+function takePhoto() {
+	snap.currentTime = 0;
+	snap.play();
+
+	const data = canvas.toDataURL('image/jpeg');
+	const link = document.createElement('a');
+	link.href = data;
+	link.setAttribute('download', 'handsome');
+	link.innerHTML = `<img src="${data}" alt="Picture" />`;
+	strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels){
+	for(var i = 0; i < pixels.data.length; i+= 4){
+		pixels[i] = pixels[i + 0] + 100;
+		pixels[i+1] = pixels[i+1] - 50;
+		pixels[i + 2] = pixels[i+2] * 0.5;
+	}
+	return pixels;
+}
+
+function rgbSplit(pixels){
+	for(var i = 0; i < pixels.data.length; i+= 4){
+		pixels[i - 150] = pixels[i+ 0]
+		pixels[i + 500] = pixels[i+1] 
+		pixels[i + -550] = pixels[i+2] 
+	}
+	return pixels;
+}
+
+function greenScreen(pixels){
+	const levels = {};
+
+	document.querySelectorAll('.rgb input').forEach((input) => {
+		levels[input.name] = input.value;
+	});
+
+	for(var i = 0; i < pixels.data.length; i+= 4){
+		red = pixels.data[i+0];
+		green = pixels.data[i+1];
+		blue = pixels.data[i+2];
+		alpha = pixels.data[i+3];
+
+		if(red >= levels.rmin && green >= levels.grmin &&
+		   blue >= levels.bmin && red <= levels.rmax &&
+		   green <= levels.gmax && blue <= levels.bmax) {
+			pixels.data[i + 3 ] = 0;
+		}
+		
+	}
+
+	return pixels;
+}
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
